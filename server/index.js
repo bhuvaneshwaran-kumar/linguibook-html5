b5f6e7f7ee180794cb776ee22ff1676613df9cb5
Bhuvi <bhuvaneshwarankumar2000@gamil.com>
2024-03-25 17:58:27 +0530

    Bhuvan ft! - settedup mongoDB connections

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
+require("dotenv").config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const port = 3000;
+const { PORT, DB_URL } = process.env;
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
-});
+const startApp = async () => {
+  try {
+    const dbStatus = await mongoose.connect(DB_URL);
+    if (dbStatus) { 
+      app.listen(PORT, () => {
+        console.log(`Server is listening at htt p://localhost:${PORT}`);
+      });
+    } else {
+      throw new Error(`Unable to connect to db ${DB_URL}`);
+    }
+  } catch (Err) {
+    console.log(Err);
+  }
+}
+
+startApp();
+
